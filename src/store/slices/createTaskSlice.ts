173972226131969dee4e283dea28631eb4d2f0e7
DiarCode <diar_beg64@mail.ru>
2023-01-24 20:59:30 +0600

    todos update

--- src/store/slices/createTaskSlice.ts
+++ src/store/slices/createTaskSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
 // Define a type for the slice state
