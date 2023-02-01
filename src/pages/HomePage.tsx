6f332ec452b58c37947c8e87b5e55df346ed9389
Diar Begisbayev <diar_beg64@mail.ru>
2023-02-01 12:11:03 +0600

    images added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,7 +11,6 @@ const CATS_IMAGES = [
   "https://www.rd.com/wp-content/uploads/2021/04/GettyImages-540542926-scaled-e1619016093503.jpg",
   "https://i1.sndcdn.com/artworks-cNp3aSmzEI1zkaHw-WPJarQ-t500x500.jpg",
   "https://images.unsplash.com/photo-1611267254323-4db7b39c732c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y3V0ZSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80",
-
 ];
 
 const HomePage = () => {
@@ -44,7 +43,11 @@ const HomePage = () => {
   const authRendered = (
     <div className="h-full flex items-center justify-center flex-col gap-y-7">
       <div className="flex items-center justify-center">
-        <img className="w-[300px] max-h-[350px] object-cover rounded-3xl" src={homeImage} alt="cat" />
+        <img
+          className="w-[300px] max-h-[350px] object-cover rounded-3xl"
+          src={homeImage}
+          alt="cat"
+        />
       </div>
       <div>
         <p className="text-[#406ffa] font-semibold text-3xl mb-4 uppercase text-center">
@@ -60,7 +63,7 @@ const HomePage = () => {
 
   return (
     <Layout>
-      <div className="w-4/5 h-4/5 bg-gray-100 rounded-lg p-7">
+      <div className="w-4/5 h-4/5 bg-gray-100 rounded-lg p-7 bg-[url(./assets/background.jpg)]">
         <div className="flex justify-end pb-7">
           <Navbar />
         </div>
