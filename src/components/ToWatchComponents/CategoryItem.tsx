1006a19a08c0fc4d63a1d85034fa8d3a3e638247
Diar Begisbayev <diar_beg64@mail.ru>
2023-02-03 19:21:12 +0600

    imports updated

--- src/components/ToWatchComponents/CategoryItem.tsx
+++ src/components/ToWatchComponents/CategoryItem.tsx
@@ -3,7 +3,6 @@ import { useAppDispatch, useAppSelector } from "../../hooks/redux.hooks";
 import towatchesSliceActions from "../../store/slices/towatchSlice";
 import { getTowatchesByCategory } from "../../api/towatches/towatches";
 import { selectAuthUser } from "../../store/slices/authSlice";
-import { useNavigate } from "react-router-dom";
 import { IToWatchCategory } from "../../types/towatch/towatch_category.type";
 
 interface CategoryItemProps {
